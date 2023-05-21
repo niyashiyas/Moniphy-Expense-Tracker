2127b2a30180b1d394cfc09504574aa1f0170323
Niya Shiyas <niyashiyas03@gmail.com>
2023-05-21 12:00:10 +0530

    components added

---
+++ frontend/src/Components/Expenses/Expenses.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function Expenses() {
+  return <ExpensesStyled>Expenses</ExpensesStyled>;
+}
+const ExpensesStyled = styled.div``;
+export default Expenses;
