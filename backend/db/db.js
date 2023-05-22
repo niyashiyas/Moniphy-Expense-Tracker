de83a6f674790787162ec48228b46062bfbaccb0
Niya Shiyas <niyashiyas03@gmail.com>
2023-05-22 21:53:39 +0530

    server issues

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,14 @@
 var {mongoose} = require("mongoose");
 var {config} = require("dotenv");
+const MONGO_URL =
+  "mongodb+srv://niya03:QSulPRwjjsBKBwPq@expensetracker.dti2am3.mongodb.net/?retryWrites=true&w=majority;";
 
 config();
 
 const db = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(MONGO_URL);
     console.log("DB connected");
   } catch (error) {
     console.log("error: " + error);
