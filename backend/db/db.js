8bda041bc1866835386882681e7efb1ba57fb26d
Niya Shiyas <niyashiyas03@gmail.com>
2023-05-31 13:26:36 +0530

    changes added

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,14 +1,12 @@
 var {mongoose} = require("mongoose");
 var {config} = require("dotenv");
-const MONGO_URL =
-  "mongodb+srv://niya03:QSulPRwjjsBKBwPq@expensetracker.dti2am3.mongodb.net/?retryWrites=true&w=majority;";
 
 config();
 
 const db = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("DB connected");
   } catch (error) {
     console.log("error: " + error);
